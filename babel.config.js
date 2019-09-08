da89e46c9560a94bd1b0d66b24b811b3fdd40dc1
YogliB <yogiboaron@gmail.com>
2019-09-08 23:23:11 +0300

    Experiment with Babel dynamic imports

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  include: ['**/**/*.js', '**/**/*.mjs', '**/**/*.html', '**/**/*.svelte'],
-  plugins: [
-    '@babel/plugin-syntax-dynamic-import',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        useESModules: true,
-      },
-    ],
-  ],
-  presets: ['@babel/preset-env'],
-  ignore: ['node_modules/**'],
+	include: ['**/**/*.js', '**/**/*.mjs', '**/**/*.html', '**/**/*.svelte'],
+	plugins: [
+		'@babel/plugin-syntax-dynamic-import',
+		[
+			'@babel/plugin-transform-runtime',
+			{
+				useESModules: true,
+			},
+			'@babel/plugin-syntax-dynamic-import',
+		],
+	],
+	presets: ['@babel/preset-env'],
+	ignore: ['node_modules/**'],
 };
