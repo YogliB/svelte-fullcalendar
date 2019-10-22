93362b97ef6c92aa5b7e362a7e761dc6715886f1
YogliB <yogiboaron@gmail.com>
2019-10-22 09:14:21 +0300

    Remove testing file

--- tests/FullCalendar.spec.js
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/svelte';
-import FullCalendar from '../src/components/FullCalendar.svelte';
-import daygridPlugin from '@fullcalendar/daygrid';
-import '@testing-library/jest-dom/extend-expect';
-
-const PLUGINS = [daygridPlugin];
-const NOW_DATE = new Date();
