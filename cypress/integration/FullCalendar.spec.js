01b19f3a12118588c304681eac3b412502346318
Yogev Boaron Ben-Har <yogiboaron@gmail.com>
2019-12-09 23:06:04 +0200

    Start adding tests

---
+++ cypress/integration/FullCalendar.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="Cypress" />
+
+context('Actions', () => {
+	beforeEach(() => {
+		cy.visit('http://localhost:5000');
+	});
+
+	it('Should render without crashing', () => {});
+
+	it('Should have updatable props', () => {});
+
+	it('Should accept a callback', () => {});
+
+	it('Should expose an API', function() {});
+});
