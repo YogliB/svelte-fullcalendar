b0d31024e89916f8ce52ae14a2f5c82ed8820ca8
YogliB <yogiboaron@gmail.com>
2020-09-09 20:28:07 +0300

    replace config

--- examples/svelte/rollup.config.js
+++ examples/svelte/rollup.config.js
@@ -1,76 +1,79 @@
-import commonjs from "rollup-plugin-commonjs";
-import livereload from "rollup-plugin-livereload";
-import pkg from "../../packages/svelte-fullcalendar/package.json";
-import resolve from "rollup-plugin-node-resolve";
-import svelte from "rollup-plugin-svelte";
-import postcss from "rollup-plugin-postcss";
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
 
-const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, "$3")
-	.replace(/^\w/, (m) => m.toUpperCase())
-	.replace(/-\w/g, (m) => m[1].toUpperCase());
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn(
+				'npm',
+				['run', 'start', '--', '--dev'],
+				{
+					stdio: ['ignore', 'inherit', 'inherit'],
+					shell: true,
+				}
+			);
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		},
+	};
+}
 
 export default {
-	input: "src/main.js",
+	input: 'src/main.js',
 	output: {
 		sourcemap: true,
-		format: "iife",
-		name: "app",
-		file: "public/build/bundle.js",
+		format: 'iife',
+		name: 'app',
+		file: 'public/build/bundle.js',
 	},
 	plugins: [
 		svelte({
 			// enable run-time checks when not in production
-			dev: true,
+			dev: !production,
 			// we'll extract any component CSS out into
-			// a separate file — better for performance
+			// a separate file - better for performance
 			css: (css) => {
-				css.write("public/build/bundle.css");
+				css.write('bundle.css');
 			},
 		}),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
+		// some cases you'll need additional configuration -
 		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: ["svelte", "@fullcalendar/common"],
+			dedupe: ['svelte'],
 		}),
 		commonjs(),
-		postcss(),
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
-		serve(),
+		!production && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
-		livereload("public"),
+		!production && livereload('public'),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser(),
 	],
 	watch: {
 		clearScreen: false,
 	},
 };
-
-function serve() {
-	let started = false;
-
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
-
-				require("child_process").spawn(
-					"npm",
-					["run", "start", "--", "--dev"],
-					{
-						stdio: ["ignore", "inherit", "inherit"],
-						shell: true,
-					}
-				);
-			}
-		},
-	};
-}
