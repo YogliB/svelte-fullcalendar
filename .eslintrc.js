f7427336031ce907bd16328d8fec306d76be7336
YogliB <yogiboaron@gmail.com>
2020-09-11 14:10:03 +0300

    add eslint & auto lint&format & add actions

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	env: {
+		browser: true,
+		node: true,
+		es6: true,
+		'cypress/globals': true,
+	},
+	extends: ['eslint:recommended', 'plugin:cypress/recommended', 'prettier'],
+	overrides: [
+		{
+			files: ['**/*.svelte'],
+			processor: 'svelte3/svelte3',
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 2019,
+		sourceType: 'module',
+	},
+	plugins: ['svelte3', 'cypress'],
+};
