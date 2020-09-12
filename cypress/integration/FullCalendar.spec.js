7dde20001913374302a127547798d2b3bd50597b
YogliB <yogiboaron@gmail.com>
2020-09-12 10:18:23 +0300

    add tests

---
+++ cypress/integration/FullCalendar.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="Cypress" />
+
+import moment from 'moment';
+
+context('Actions', () => {
+	beforeEach(() => {
+		cy.visit('http://localhost:5000');
+	});
+
+	it('Should render without crashing', () => {
+		cy.get('.fc-toolbar').should('exist');
+		cy.get('.fc-view-container').should('exist');
+	});
+
+	it('Should have updatable props', () => {
+		cy.findByText('Sat').should('exist');
+		cy.findByText('toggle weekends').click();
+		cy.findByText('Sat').should('not.exist');
+	});
+
+	it('Should accept a callback', () => {
+		const date = new Date();
+		const formatedDate = moment(date).format('YYYY-MM-DD');
+
+		cy.get(`.fc-day[data-date=${formatedDate}]`).click();
+		cy.findByText('New Event').should('exist');
+	});
+
+	it('Should expose an API', () => {
+		cy.findByText('go to a date in the past').click();
+		cy.findByText('January 2000').should('exist');
+	});
+});
