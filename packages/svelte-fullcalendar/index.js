f0e4916cb17849e6ffb34b7101c041be50795e86
YogliB <yogiboaron@gmail.com>
2021-07-17 19:56:16 +0300

    chore(package): add comment

--- packages/svelte-fullcalendar/index.js
+++ packages/svelte-fullcalendar/index.js
@@ -1,3 +1,5 @@
 export { default } from './src/FullCalendar.svelte';
 export { default as Draggable } from './src/Draggable.svelte';
+
+// make all of FC's utils/types available
 export * from '@fullcalendar/core';
