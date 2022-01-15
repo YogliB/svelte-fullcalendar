bdb1cbe9837d0d45f49cd5702dc124f697972847
Yogev Boaron Ben-Har <me@yogev.dev>
2022-01-15 08:47:02 +0200

    refactor: simplify code for filter null props

--- packages/svelte-fullcalendar/src/helpers.js
+++ packages/svelte-fullcalendar/src/helpers.js
@@ -2,7 +2,7 @@ export const filterNullProps = (obj) => {
 	let result = {};
 
 	for (const key in obj) {
-		if (obj[key] !== null && obj[key] !== undefined) result[key] = obj[key];
+		if (obj[key] != null) result[key] = obj[key];
 	}
 
 	return result;
